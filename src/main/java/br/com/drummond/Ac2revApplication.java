7c936d1ae14ec936042f28cff135f13ce9c9bb65
paulaleticia21 <paula.leticia.braz@gmail.com>
2019-10-09 21:54:33 -0300

    Revisao Ac2

---
+++ src/main/java/br/com/drummond/Ac2revApplication.java
@@ -0,0 +1,13 @@
+package br.com.drummond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ac2revApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ac2revApplication.class, args);
+	}
+
+}
